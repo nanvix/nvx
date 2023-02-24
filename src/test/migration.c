0b81ef9073851c6af634c85255a7a4088fa11e73
nicolasvanz <nicolas.vanz+github@outlook.com>
2023-02-24 15:29:50 -0300

    [test] add migration test scripts

--- src/test/migration.c
+++ src/test/migration.c
@@ -17,33 +17,155 @@
 #define PORT 0
 #define PORT2 10
 
+#define CIRCULAR_SLAVE_FIRST_SENDER (SLAVE_NODENUM)
+#define CIRCULAR_SLAVE_LAST_SENDER (SLAVE_NODENUM + 15)
+#define CIRCULAR_SLAVE_LAST_RECEIVER (SLAVE_NODENUM)
+
+#define THREADS_SLAVE_SENDER (SLAVE_NODENUM)
+#define THREADS_SLAVE_RECEIVER (SLAVE_NODENUM + 1)
+
+#if defined(TEST_CIRCULAR_MIGRATION)
+	#define SLAVE_MAILBOX_CONTROLLER CIRCULAR_SLAVE_LAST_RECEIVER
+	#define NEED_SETUP_TIME_SYNC    0
+#elif defined(TEST_MULTIPLE_THREADS)
+	#define SLAVE_MAILBOX_CONTROLLER THREADS_SLAVE_RECEIVER
+	#define NEED_SETUP_TIME_SYNC    1
+#elif defined(TEST_PARALLEL_MIGRATION)
+	#define SLAVE_MAILBOX_CONTROLLER (SLAVE_NODENUM + 1)
+	#define NEED_SETUP_TIME_SYNC    1
+#endif
 
 int nodes[NR_NODES];
-
-#define SLAVE_FIRST_SENDER (SLAVE_NODENUM)
-#define SLAVE_LAST_SENDER (SLAVE_NODENUM + 15)
-#define SLAVE_LAST_RECEIVER (SLAVE_NODENUM)
-#define SLAVE_MAILBOX_CONTROLLER SLAVE_LAST_RECEIVER
-
-int done = 0;
+int * cnodes = &nodes[1];
+int done;
 unsigned int global = 0;
 int out __attribute__((section(".libnanvix.data")));
+int syncin __attribute__((section(".libnanvix.data")));
+int syncout __attribute__((section(".libnanvix.data")));
 
 PRIVATE inline void slave_wait()
 {
 	while(1);
 }
 
-PRIVATE void slave_fn()
+PRIVATE void *thread_work(void *arg)
 {
-	int syncin, syncout;
+	UNUSED(arg);
+
+	int *i = &done;
+	while(!(*i))
+	{
+		dcache_invalidate();
+	};
+
+	return NULL;
+}
+
+PRIVATE void master_fn()
+{
+	uint64_t t1, setup;
+
+	test_assert((syncin = ksync_create(nodes, NR_NODES, SYNC_ALL_TO_ONE)) >= 0);
+	test_assert((syncout = ksync_open(nodes, NR_NODES, SYNC_ONE_TO_ALL)) >= 0);
+
+	test_assert(ksync_signal(syncout) == 0);
+	test_assert(ksync_wait(syncin) == 0);
+
+	perf_start(0, PERF_CYCLES);
+
+	#if NEED_SETUP_TIME_SYNC
+		test_assert(ksync_wait(syncin) == 0);
+		test_assert(ksync_signal(syncout) == 0);
+		setup = perf_read(0);
+	#endif
+
+	test_assert(ksync_signal(syncout) == 0);
+	test_assert(ksync_wait(syncin) == 0);
+
+	perf_stop(0);
+	#if NEED_SETUP_TIME_SYNC
+		t1 = perf_read(0) - setup;
+	#else
+		t1 = perf_read(0);
+	#endif
+
+	kprintf("time: %l", t1);
+}
+
+PRIVATE inline int parallel_node_index(void)
+{
+	return knode_get_num() - SLAVE_NODENUM;
+}
+
+PRIVATE inline int parallel_receveiver_node_index()
+{
+	return ((NR_NODES - 1) / 2) + parallel_node_index();
+}
+
+PRIVATE int parallel_is_sender()
+{
+	if (parallel_node_index() < (NR_NODES - 1) / 2)
+		return (1);
+	return (0);
+
+}
+
+PRIVATE void slave_fn_parallel()
+{
+	kthread_t tids[TESTS_NTHREADS];
+	unsigned * pg;
+	const unsigned magic = 0xdeadbeef;
+
+	test_assert((syncin = ksync_create(nodes, NR_NODES, SYNC_ONE_TO_ALL)) >= 0);
+	test_assert((syncout = ksync_open(nodes, NR_NODES, SYNC_ALL_TO_ONE)) >= 0);
+
+	test_assert(ksync_wait(syncin) == 0);
+	test_assert(ksync_signal(syncout) == 0);
+
+	done = 0;
+	if (parallel_is_sender())
+	{
+		for (int i = 0; i < TESTS_NTHREADS; i++)
+			KASSERT(kthread_create(&tids[i], thread_work, NULL) == 0);
+
+		for (int i = 0; i < NPAGES; i++)
+		{
+			pg = (unsigned  *)(UBASE_VIRT) + i * PAGE_SIZE;
+
+			KASSERT(page_alloc(VADDR(pg)) == 0);
+
+			/* Write. */
+			for (size_t j = 0; j < PAGE_SIZE/sizeof(unsigned); j++)
+				pg[j] = magic;
+
+		}
+		test_assert(ksync_signal(syncout) == 0);
+		test_assert(ksync_wait(syncin) == 0);
+		kmigrate_to(cnodes[parallel_receveiver_node_index()]);
+	}
+	else
+	{
+		test_assert(ksync_signal(syncout) == 0);
+		test_assert(ksync_wait(syncin) == 0);
+		slave_wait();
+	}
+
+	test_assert(ksync_wait(syncin) == 0);
+	test_assert(ksync_signal(syncout) == 0);
+	done = 1;
+	for (int i = 0; i < TESTS_NTHREADS; i++)
+		KASSERT(kthread_join(tids[i], NULL) == 0);
+}
+
+PRIVATE void slave_fn_circular()
+{
 	char dummy = 'd';
 	int local = knode_get_num();
 
 	unsigned *pg;
 	const unsigned magic = 0xdeadbeef;
 
-	if (local == SLAVE_FIRST_SENDER)
+	if (local == CIRCULAR_SLAVE_FIRST_SENDER)
 	{
 		pg = (unsigned  *)(UBASE_VIRT);
 
@@ -72,15 +194,15 @@ PRIVATE void slave_fn()
 		test_assert(kmailbox_write(out, &dummy, sizeof(char)) == sizeof(char));
 	}
 
-	if (local != SLAVE_FIRST_SENDER)
+	if (local != CIRCULAR_SLAVE_FIRST_SENDER)
 		slave_wait();
 
-	for (int remote = SLAVE_NODENUM + 1; remote <= SLAVE_LAST_SENDER; remote++)
+	for (int remote = SLAVE_NODENUM + 1; remote <= CIRCULAR_SLAVE_LAST_SENDER; remote++)
 	{
 		if (knode_get_num() == remote - 1)
 		{
-			kprintf("migrating to %d", remote);
 			#if DEBUG
+			kprintf("migrating to %d", remote);
 			#endif
 
 			kmigrate_to(remote);
@@ -99,43 +221,77 @@ PRIVATE void slave_fn()
 		/* wait until last sender revive this cluster */
 		slave_wait();
 
-	if (knode_get_num() == SLAVE_LAST_SENDER)
-		kmigrate_to(SLAVE_LAST_RECEIVER);
+	if (knode_get_num() == CIRCULAR_SLAVE_LAST_SENDER)
+		kmigrate_to(CIRCULAR_SLAVE_LAST_RECEIVER);
 
 
 	if (knode_get_num() == SLAVE_MAILBOX_CONTROLLER)
 		test_assert(kmailbox_write(out, &dummy, sizeof(char)) == sizeof(char));
 }
 
-PRIVATE void master_fn()
+PRIVATE void slave_fn_threads()
 {
-	int in, syncin, syncout;
-	char dummy;
-	uint64_t t1, error;
-	test_assert((syncin = ksync_create(nodes, NR_NODES, SYNC_ALL_TO_ONE)) >= 0);
-	test_assert((syncout = ksync_open(nodes, NR_NODES, SYNC_ONE_TO_ALL)) >= 0);
+	unsigned * pg;
+	kthread_t tids[TESTS_NTHREADS];
+	const unsigned magic = 0xdeadbeef;
 
-	test_assert((in = kmailbox_create(MASTER_NODENUM, PORT2)) >= 0);
+	test_assert((syncin = ksync_create(nodes, NR_NODES, SYNC_ONE_TO_ALL)) >= 0);
+	test_assert((syncout = ksync_open(nodes, NR_NODES, SYNC_ALL_TO_ONE)) >= 0);
 
-	test_assert(ksync_signal(syncout) == 0);
 	test_assert(ksync_wait(syncin) == 0);
+	test_assert(ksync_signal(syncout) == 0);
 
-	test_assert(kmailbox_read(in, &dummy, sizeof(char)) == sizeof(char));
-	perf_start(0, PERF_CYCLES);
+	done = 0;
 
-	test_assert(kmailbox_read(in, &dummy, sizeof(char)) == sizeof(char));
-	t1 = perf_read(0);
+	if (knode_get_num() == THREADS_SLAVE_RECEIVER)
+	{
+		test_assert(ksync_signal(syncout) == 0);
+		test_assert(ksync_wait(syncin) == 0);
+		slave_wait();
+	}
 
-	error = t1;
+	if (knode_get_num() == THREADS_SLAVE_SENDER)
+	{
+		/* create threads */
+		for (int i = 0; i < TESTS_NTHREADS; i++)
+			KASSERT(kthread_create(&tids[i], thread_work, NULL) == 0);
 
-	test_assert(kmailbox_read(in, &dummy, sizeof(char)) == sizeof(char));
-	perf_stop(0);
-	t1 = perf_read(0);
+		for (int i = 0; i < NPAGES; i++)
+		{
+			pg = (unsigned  *)(UBASE_VIRT) + i * PAGE_SIZE;
 
-	kprintf("time: %l", t1 - error);
+			KASSERT(page_alloc(VADDR(pg)) == 0);
+
+			/* Write. */
+			for (size_t j = 0; j < PAGE_SIZE/sizeof(unsigned); j++)
+				pg[j] = magic;
+
+		}
+		test_assert(ksync_signal(syncout) == 0);
+		test_assert(ksync_wait(syncin) == 0);
+		kmigrate_to(THREADS_SLAVE_RECEIVER);
+	}
+
+	done = 1;
+
+	#if DEBUG
+
+	for (int i = 0; i < NPAGES; i++)
+	{
+		pg = (unsigned  *)(UBASE_VIRT) + i * PAGE_SIZE;
+
+		for (size_t j = 0; j < PAGE_SIZE/sizeof(unsigned); j++)
+			KASSERT(pg[j] == magic);
+	}
+
+	#endif
+
+	test_assert(ksync_wait(syncin) == 0);
+	test_assert(ksync_signal(syncout) == 0);
+
+	for (int i = 0; i < TESTS_NTHREADS; i++)
+		kthread_join(tids[i], NULL);
 }
-
-
 
 PRIVATE void test_api_migration()
 {
@@ -145,7 +301,14 @@ PRIVATE void test_api_migration()
 	for (int i = 1; i < NR_NODES; i++)
 		nodes[i] = SLAVE_NODENUM + i - 1;
 
-	fn = knode_get_num() == MASTER_NODENUM ? master_fn : slave_fn;
+	#if defined(TEST_CIRCULAR_MIGRATION)
+	fn = knode_get_num() == MASTER_NODENUM ? master_fn : slave_fn_circular;
+	#elif defined(TEST_MULTIPLE_THREADS)
+	fn = knode_get_num() == MASTER_NODENUM ? master_fn : slave_fn_threads;
+	#elif defined(TEST_PARALLEL_MIGRATION)
+	fn = knode_get_num() == MASTER_NODENUM ? master_fn : slave_fn_parallel;
+	#endif
+
 	fn();
 }
 
@@ -153,7 +316,6 @@ PRIVATE struct test migration_tests_api[] = {
 	{ test_api_migration,             "[test][migration][api] migration          [passed]" },
 	{ NULL,                            NULL                                             },
 };
-
 
 void test_migration(void)
 {
